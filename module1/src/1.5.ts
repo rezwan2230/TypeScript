2a25a54d07ec37a460b9d2fdd5e9884b3535c9bb
rezwan2230 <rezwan2230@gmail.com>
2025-08-02 17:13:16 +0600

    added referance type and literal type

---
+++ module1/src/1.5.ts
@@ -0,0 +1,15 @@
+//Reference Type -->Object
+
+const user: {
+  company: "Programming Hero"; //literal type
+  firstName: string;
+  middelName?: string;
+  lastName: string;
+  isMarried: boolean;
+} = {
+  firstName: "Rezwan",
+  middelName: "islam",
+  lastName: "repon",
+  isMarried: true,
+  company: "Programming Hero",
+};
