8224a678101102326b6ad5dc7bca64034f37123a
rezwan2230 <rezwan2230@gmail.com>
2025-08-12 23:14:20 +0600

    exploring oop concept for typescript, class and object

---
+++ module3/src/3.1.ts
@@ -0,0 +1,21 @@
+//todo-> oop->class
+
+class Animal {
+  constructor(
+    public name: string,
+    public species: string,
+    public sound: string
+  ) {}
+  
+  //!arrow function are not working here couse if we write an arrow function here we can't use "this" keyword.
+  makeSound() {
+    console.log(`The ${this.name} says ${this.sound}`);
+  }
+}
+
+const dog = new Animal("Puppy", "Shepard", "Gheu Gheu");
+const cat = new Animal("Simba", "Parshian", "Meaw Meaw");
+console.log(dog);
+dog.makeSound();
+console.log(cat);
+cat.makeSound();
